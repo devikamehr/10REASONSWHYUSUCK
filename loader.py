7188e6451dad2eba0005763371218adb4780c7f0
devikamehr <devikamehr@gmail.com>
2022-04-18 16:57:10 -0700

    work

---
+++ loader.py
@@ -0,0 +1,130 @@
+
+   
+# coding: utf-8
+#
+# the top line, above, is important --
+# it ensures that Python will be able to use this file,
+# even if you paste in text with Unicode characters
+# (beyond ASCII)
+# for an more expansive example of such a file, see
+#    http://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-demo.txt
+#
+# OK! Now we're ready for hw10pr3.py ...
+#
+#
+# Name:
+#
+import random
+def wc(filename):
+    """Word-counting function.  Opens a file named 'filename', reads
+       it, and returns the number of words in the file.
+       Example: wc('a.txt')
+    """
+    #
+    # First we have to "open" the file (just like opening a book so you
+    # can read it).  We ask Python to use the "latin1" encoding, because it
+    # accepts more characters ASCII.  If you have a REALLY adventurous file,
+    # you might need to use the "utf-8" encoding.
+    #
+    f = open(filename, encoding = 'latin1')
+
+    #
+    # Now read the entire contents of the file into a string named "text",
+    # and then close it.  Note that this approach only works for relatively
+    # small files; bigger ones should be read one line at a time by calling
+    # 'f.readline()' in a loop.
+    #
+    text = f.read()
+    f.close()
+
+    #
+    # The text of the file is one long string.  Use "split" to divide
+    # it up into "words" at whitespace boundaries.
+    #
+    LoW = text.split()    
+
+    #
+    # LoW is a list of words, so its length is the word count.
+    #
+    return len(LoW)
+
+def vc(filename):
+    """Vocabulary-counting function.  Opens a file named 'filename',
+       reads it, and breaks it into words.  Then, for each word,
+       counts how many times it occurs.  Prints a message giving the
+       number of distinct words in the file, and then returns a
+       dictionary in which each word is a key and the word's frequency
+       is the value.
+       Example: vc('a.txt') might print 3 and return
+       {'I': 2, 'love': 3, 'spam': 42}
+    """
+    f = open(filename, encoding = 'latin1')
+    text = f.read()
+    f.close()
+
+    LoW = text.split()    
+    print("There are", len(LoW), "words.")
+
+    d = {}
+    for w in LoW:
+        if w not in d:
+            d[w] = 1
+        else:
+            d[w] += 1
+            
+    print("There are", len(d), "*distinct* words.\n")
+    return d   # this way we can use the data in d later!
+
+# function #1
+#
+def createDictionary(filename):
+    f = open(filename, encoding = 'latin1')
+    text = f.read()
+    f.close()
+
+    LoW = text.split()    
+    d = {}
+    pw = '$'
+
+    for nw in LoW:
+        if pw not in d:
+            d[pw] = [nw]
+        else:
+            d[pw] += [nw]
+                    
+
+        pw = nw
+        if pw[-1] == "!" or pw[-1] == "." or pw[-1] == "?":
+            pw = '$'
+
+    return d
+
+    # then check for whether that new pw ends in 
+    # punctuation -- if it _does_ then set pw = '$
+
+
+
+# function #2
+#
+def generateText(d, N):
+    final = " "
+    pointer = "$"
+    #words = fw
+    #length = len(d)
+    # pointer = random.choice(d[1,length])
+    for i in range(N):
+        pointer = random.choice(d[pointer])
+        final += " " + pointer
+        #final += " " + newWord[pointer]
+        nextWord = pointer[-1]
+        if nextWord == "!" or nextWord == "." or nextWord == "?":
+            pointer = '$'
+
+    return final[1:]
+
+def createPhrases():
+    d = createDictionary('info.txt')
+    l = generateText(d, 56).split('.')
+    return l
+
+
--- memes.py
+++ memes.py
@@ -5,14 +5,18 @@ from pytesseract import pytesseract
 import os
 import numpy as np
 import random
+import loader
+from string import ascii_letters
+import textwrap
 
+list_of_text = loader.createPhrases()
   
 # Defining paths to tesseract.exe
 # and the image we would be using
 # path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-for i in range(30):
-    path_to_tesseract = r"/usr/local/Cellar/tesseract/5.1.0/bin/tesseract"
-    image_path = r"/Users/devikachipalkatti/Documents/meme/text"
+for i in range(6):
+    # path_to_tesseract = r"/usr/local/Cellar/tesseract/5.1.0/bin/tesseract"
+    # image_path = r"/Users/devikachipalkatti/Documents/meme/text"
     randomImage_path = r"/Users/devikachipalkatti/Documents/meme/reactions"
     final_path =r"/Users/devikachipalkatti/Documents/meme/draft"
 
@@ -22,37 +26,46 @@ for i in range(30):
     ])
     randomImage_path = r"/Users/devikachipalkatti/Documents/meme/reactions/" + random_filename
 
-    random_text = random.choice([
-        x for x in os.listdir(image_path)
-        if os.path.isfile(os.path.join(image_path, x))
-    ])
-    image_path = r"/Users/devikachipalkatti/Documents/meme/text/" + random_text
+    random_text = random.choice(list_of_text)
+    # random_text = random.choice([
+    #     x for x in os.listdir(image_path)
+    #     if os.path.isfile(os.path.join(image_path, x))
+    # ])
+    # image_path = r"/Users/devikachipalkatti/Documents/meme/text/" + random_text
 
 
     # Opening the image & storing it in an image object
-    img = Image.open(image_path)
+    # img = Image.open(image_path)
     randomImage = Image.open(randomImage_path)
     # Providing the tesseract executable
     # location to pytesseract library
-    pytesseract.tesseract_cmd = path_to_tesseract
+    # pytesseract.tesseract_cmd = path_to_tesseract
     
     # Passing the image object to image_to_string() function
     # This function will extract the text from the image
-    text = pytesseract.image_to_string(img)
+    # text = pytesseract.image_to_string(img)
 
     clr = tuple([np.random.choice(range(256)) for i in range(3)])
-    newMeme=  Image.new('RGB', (700, 700), color = clr)
-    (width, height) = (randomImage.width // 4, randomImage.height // 4)
-    randomImage = randomImage.resize((height, width))
-    newMeme.paste(randomImage, (50,200))
+    # newMeme=  Image.new('RGB', (700, 700), color = clr)
+    # (width, height) = (randomImage.width // 4, randomImage.height // 4)
+    # randomImage = randomImage.resize((height, width))
+    # newMeme.paste(randomImage, (50,200))
 
     # fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 15)
-    fnt = ImageFont.truetype(font='LuckiestGuy-Regular.ttf', size=35)
+    font = ImageFont.truetype(font='LuckiestGuy-Regular.ttf', size=30)
+    avg_char_width = sum(font.getsize(char)[0] for char in ascii_letters) / len(ascii_letters)
+    max_char_count = int(randomImage.size[0] * .618 / avg_char_width)
+    text = textwrap.fill(text=random_text, width=max_char_count)
 
-    d = ImageDraw.Draw(newMeme)
-    d.text((100,100), text[:-1], font=fnt, fill=(255, 255, 0))
+
+    draw = ImageDraw.Draw(randomImage)
+    # draw.text((randomImage.width//2,randomImage.height//2), text, font=font, fill=(0, 0, 0))
+    draw.text(xy=(randomImage.size[0]/2, randomImage.size[1] / 2), text=text, font=font, fill='#000000', anchor='mm')
+
+
     filenum = str(random.randint(0,10000))
-    newMeme = newMeme.save(f"{final_path}/image" + filenum + ".png")
+    randomImage.show()
+    randomImage = randomImage.save(f"{final_path}/image" + filenum + ".png")
 
     # Displaying the extracted text
     # print(text[:-1])
