f9fe03040a3e2ed469fb8aa661cc69980ddd05e9
devikamehr <devikamehr@gmail.com>
2022-04-19 15:10:22 -0700

    Fun funky images.

--- loader.py
+++ loader.py
@@ -124,7 +124,7 @@ def generateText(d, N):
 
 def createPhrases():
     d = createDictionary('info.txt')
-    l = generateText(d, 56).split('.')
+    l = generateText(d, 65).split('.')
     return l
 
 
--- memes.py
+++ memes.py
@@ -9,63 +9,40 @@ import loader
 from string import ascii_letters
 import textwrap
 
+# Grab list of phrases from markov model
 list_of_text = loader.createPhrases()
   
-# Defining paths to tesseract.exe
-# and the image we would be using
-# path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-for i in range(6):
-    # path_to_tesseract = r"/usr/local/Cellar/tesseract/5.1.0/bin/tesseract"
-    # image_path = r"/Users/devikachipalkatti/Documents/meme/text"
+
+for i in range(20):
+    # Grab paths
     randomImage_path = r"/Users/devikachipalkatti/Documents/meme/reactions"
     final_path =r"/Users/devikachipalkatti/Documents/meme/draft"
 
+    # Pick a random backgorund file path.
     random_filename = random.choice([
         x for x in os.listdir(randomImage_path)
         if os.path.isfile(os.path.join(randomImage_path, x))
     ])
     randomImage_path = r"/Users/devikachipalkatti/Documents/meme/reactions/" + random_filename
 
+    # Pick a random text and background together
     random_text = random.choice(list_of_text)
-    # random_text = random.choice([
-    #     x for x in os.listdir(image_path)
-    #     if os.path.isfile(os.path.join(image_path, x))
-    # ])
-    # image_path = r"/Users/devikachipalkatti/Documents/meme/text/" + random_text
 
-
-    # Opening the image & storing it in an image object
-    # img = Image.open(image_path)
     randomImage = Image.open(randomImage_path)
-    # Providing the tesseract executable
-    # location to pytesseract library
-    # pytesseract.tesseract_cmd = path_to_tesseract
-    
-    # Passing the image object to image_to_string() function
-    # This function will extract the text from the image
-    # text = pytesseract.image_to_string(img)
 
+    # Pick a random color and font
     clr = tuple([np.random.choice(range(256)) for i in range(3)])
-    # newMeme=  Image.new('RGB', (700, 700), color = clr)
-    # (width, height) = (randomImage.width // 4, randomImage.height // 4)
-    # randomImage = randomImage.resize((height, width))
-    # newMeme.paste(randomImage, (50,200))
+    font = ImageFont.truetype(font='LEMONMILK-BoldItalic.otf', size=30)
 
-    # fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 15)
-    font = ImageFont.truetype(font='LuckiestGuy-Regular.ttf', size=30)
+    # Text wrapping crap
     avg_char_width = sum(font.getsize(char)[0] for char in ascii_letters) / len(ascii_letters)
     max_char_count = int(randomImage.size[0] * .618 / avg_char_width)
     text = textwrap.fill(text=random_text, width=max_char_count)
 
-
+    # Draw the text on half the image.
     draw = ImageDraw.Draw(randomImage)
-    # draw.text((randomImage.width//2,randomImage.height//2), text, font=font, fill=(0, 0, 0))
     draw.text(xy=(randomImage.size[0]/2, randomImage.size[1] / 2), text=text, font=font, fill='#000000', anchor='mm')
 
-
+    # Save the image
     filenum = str(random.randint(0,10000))
-    randomImage.show()
     randomImage = randomImage.save(f"{final_path}/image" + filenum + ".png")
-
-    # Displaying the extracted text
-    # print(text[:-1])
